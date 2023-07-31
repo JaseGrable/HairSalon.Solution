798f04fbcfb5b59993b76a1c60376bfc5c13cc0e
JaseGrable <jase.grable@gmail.com>
2023-07-30 22:24:08 -0700

    Add salon context

---
+++ HairSalon/Models/SalonContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon.Models
+{
+    public class SalonContext : DbContext
+    {
+        public DbSet<Stylist> Stylists { get; set; }
+        public DbSet<Clients> Clients { get; set; }
+        public SalonContext(DbContextOptions options) : base(options) {}
+    }
+}
